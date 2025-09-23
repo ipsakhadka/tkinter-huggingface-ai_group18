48f953aa3e5e71160935c90e79a3ade9ab7d1b22
Manisha Gautam <mnsagtm123@gmail.com>
2025-09-23 20:15:47 +1000

    Installed Transformers

--- index.py
+++ index.py
@@ -3,7 +3,9 @@ from tkinter import *
 root = Tk()
 
 #creating a Label widget
-myLabel = Label(root, text="This is the starting phase for Assignment 3.")
-myLabel.pack()
+myLabel1 = Label(root, text="This is the starting phase for Assignment 3.")
+myLabel2 = Label(root, text="FOR SOFTWARE NOW!")
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
 
 root.mainloop()
