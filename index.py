aa07061cabcd450e850eeb9c700c8dc4a1b5fb12
shushil555 <shushilpaudel2014@gmail.com>
2025-10-02 12:30:17 +1000

    code refine and comments for clarity

--- index.py
+++ index.py
@@ -1,11 +1,21 @@
+#importing from tkinter
+
 from tkinter import *
+
+#creating main window
 
 root = Tk()
 
 #creating a Label widget
+#first label
 myLabel1 = Label(root, text="This is the starting phase for Assignment 3.")
+
+#second label
 myLabel2 = Label(root, text="FOR SOFTWARE NOW!")
+
+#placing labels in the window
 myLabel1.grid(row=0, column=0)
 myLabel2.grid(row=1, column=0)
 
+#lets run the application window
 root.mainloop()
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+# starting point of our project where the GUI and Hugging Face Models will be launched
+
+#importing main gui class from gui.py
 from gui import AIApp
 
 if __name__ == "__main__":
-    app = AIApp()
+    app = AIApp()               #creating an instance
+
+    #starting the tkinter main loop
     app.mainloop()
